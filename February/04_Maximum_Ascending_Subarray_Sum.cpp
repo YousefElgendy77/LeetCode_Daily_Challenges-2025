7a62d0518c6125bde367ae94cfe823770325fba3
Yousef Elgendy <elgendyyousef539@gmail.com>
2025-02-05 00:38:25 +0200

    Day 4

---
+++ February/04_Maximum_Ascending_Subarray_Sum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int sum = nums[0] , maxSum = 0;
+        for(int i = 1 ; i < (int)nums.size() ;++i)
+        {
+            if(nums[i] > nums[i-1])
+                sum += nums[i];
+            else
+            {
+                maxSum = max(maxSum , sum);
+                sum = nums[i];
+            }
+        }
+        maxSum = max(maxSum , sum);
+        return maxSum;
+    }
+};
+
+int main()
+{
+    return 0;
+}
