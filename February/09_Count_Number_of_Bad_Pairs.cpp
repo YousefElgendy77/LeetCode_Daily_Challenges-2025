6e98692a20363e83f68acd8d7f94e624ed0c05c2
Yousef Elgendy <elgendyyousef539@gmail.com>
2025-02-09 23:30:24 +0200

    Day 9

--- February/09_Count_Number_of_Bad_Pairs.cpp
+++ February/09_Count_Number_of_Bad_Pairs.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Solution {
 public:
-    long long countBadPairs(vector<int>& nums) {
+    long long countBadPairs(vector<int>& nums) { 
         unordered_map<int,int>notBad;
         int n = (int)nums.size() ;
         long long ans = 0;
@@ -15,8 +15,8 @@ public:
         for(int i = 0 ; i < n ; ++i)
         { 
             int res = 0;
-            if(notBad[nums[i]] > 1) res = notBad[nums[i]] - 1;
-            ans += (n-i-1) - res;
+            if(notBad[nums[i]] > 1) res = notBad[nums[i]] - 1; // count the bad good pairs
+            ans += (n-i-1) - res;        // subtract good bairs from bad pairs
             notBad[nums[i]]--;
         }
         return ans;
@@ -26,3 +26,18 @@ int main()
 {
     return 0;
 }
+
+// class Solution {
+//     public:
+//         long long countBadPairs(vector<int>& nums) {
+//             int n = (int)nums.size();
+//             long long ans = 0;
+//             unordered_map<int, int> occ;       // good pairs so Far
+//             for (int i = 0; i < n; ++i) {
+//                 int cur = i - nums[i];         // current pair
+//                 ans += i - occ[cur];           // ans += bad pairs so Far
+//                 occ[cur]++; 
+//             }
+//             return ans;
+//         }
+// };
