ebe1b5fb2937c9a38efc96faec02c3d61cb6322c
Yousef Elgendy <elgendyyousef539@gmail.com>
2025-02-10 23:16:05 +0200

    Day 10

--- February/10_Clear_Digits.cpp
+++ February/10_Clear_Digits.cpp
@@ -4,21 +4,17 @@ using namespace std;
 class Solution {
 public:
     string clearDigits(string s) {
-        int n = (int)s.size();
-        string ans = "";
-        for(int i = n-1 ; i >= 0 ; --i)
-        {
-            if(s[i] >= 'a' && s[i] <= 'z') ans += s[i];
-            else if(s[i] >= '0' && s[i] <= '9')
-                for(int j = i-1 ; j >= 0 ; --j)
-                    if(s[j] >= 'a' && s[j] <= 'z')
-                    {
-                        s[j] = '#';
-                        break;
-                    }
-        }
-        reverse(ans.begin() , ans.end());
-        return ans;
+        string stack = "";
+        for (char c : s) 
+            if (isdigit(c)) 
+            {
+                if (!stack.empty())    // Remove the closest non-digit character if the stack is not empty
+                    stack.pop_back();
+            } 
+            else
+                stack.push_back(c);
+        
+        return stack;
     }
 };
 int main()
