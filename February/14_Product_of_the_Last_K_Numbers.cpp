a13e56f2ccb6891ece3cd1d1165f786355b20421
Yousef Elgendy <elgendyyousef539@gmail.com>
2025-02-15 00:20:28 +0200

    Day 14

--- February/14_Product_of_the_Last_K_Numbers.cpp
+++ February/14_Product_of_the_Last_K_Numbers.cpp
@@ -25,28 +25,28 @@ public:
     }
 };
 
-//class ProductOfNumbers { // more optimized solution
-//     vector<int> productPrefix {1};
-// public:
-//     ProductOfNumbers() {
+class ProductOfNumbers { // more optimized solution
+    vector<int> productPrefix {1};
+public:
+    ProductOfNumbers() {
         
-//     }
+    }
     
-//     void add(int num) {
-//         if (num == 0) {
-//             productPrefix.clear();
-//             productPrefix.push_back(1);
-//         } else {
-//             int mul = num * productPrefix.back();
-//             productPrefix.push_back(mul);
-//         }
-//     }
+    void add(int num) {
+        if (num == 0) {
+            productPrefix.clear();
+            productPrefix.push_back(1);
+        } else {
+            int mul = num * productPrefix.back();
+            productPrefix.push_back(mul);
+        }
+    }
     
-//     int getProduct(int k) {
-//         int sz = (int)productPrefix.size();
-//         return k < sz ? productPrefix.back() / productPrefix[sz - k - 1] : 0;
-//     }
-// };
+    int getProduct(int k) {
+        int sz = (int)productPrefix.size();
+        return k < sz ? productPrefix.back() / productPrefix[sz - k - 1] : 0;
+    }
+};
 int main()
 {
     return 0;
